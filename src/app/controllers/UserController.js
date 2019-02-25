361c4896c4459546d801fbe80b44f646bdd9763e
Willian Domingos <mdwillian@gmail.com>
2019-02-25 01:54:08 -0300

    Upload avatar

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,9 +6,8 @@ class UserController {
   }
 
   async store (req, res) {
-    req.body.avatar = 'teste.jpeg'
-
-    await User.create(req.body)
+    const { filename } = req.file
+    await User.create({ ...req.body, avatar: filename })
 
     return res.redirect('/')
   }
